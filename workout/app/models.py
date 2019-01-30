a583dbc788ef934ea9691e4ea2101843ca41c126
Gram <master_fess@mail.ru>
2019-01-30 11:58:48 +0300

    +models

--- workout/app/models.py
+++ workout/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField('Name', max_length=16)
+
+
+class Day(models.Model):
+    name = models.CharField('Name', max_length=16)
+    exercises = models.ManyToManyField(Exercise, 'Exercises')
+
+
+class Plan(models.Model):
+    name = models.CharField('Name', max_length=16)
+    days = models.ManyToManyField(Day, 'Days')
+
+
+class User(AbstractUser):
+    plan = models.ForeignKey(Plan)
