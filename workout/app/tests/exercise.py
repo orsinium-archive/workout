c338004268b14ce01b6dbfe2c63aa97164d2a0c7
Gram <master_fess@mail.ru>
2019-01-30 17:06:43 +0300

    test exercises

---
+++ workout/app/tests/exercise.py
@@ -0,0 +1,65 @@
+# external
+from django.test import TestCase
+from rest_framework import status
+
+# app
+from .mixins import JWTMixin
+
+
+class UserTests(JWTMixin, TestCase):
+    def test_list(self):
+        # check list
+        response = self.client.get('/api/v1/exercises/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+        # add
+        response = self.client.post('/api/v1/exercises/', data=dict(name='running'))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # check list
+        response = self.client.get('/api/v1/exercises/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], 'running')
+
+    def test_detail(self):
+        # add
+        response = self.client.post('/api/v1/exercises/', data=dict(name='running'))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # get info
+        exercise_id = response.data[0]['id']
+        response = self.client.get('/api/v1/exercises/{}/'.format(exercise_id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], self.username)
+
+    def test_add(self):
+        # add
+        response = self.client.post('/api/v1/exercises/', data=dict(name='running'))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # check list
+        response = self.client.get('/api/v1/exercises/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_delete(self):
+        # add
+        response = self.client.post('/api/v1/exercises/', data=dict(name='running'))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # check list
+        response = self.client.get('/api/v1/exercises/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+        # delete
+        exercise_id = response.data['id']
+        response = self.client.delete('/api/v1/exercises/{}/'.format(exercise_id))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # check list
+        response = self.client.get('/api/v1/exercises/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
