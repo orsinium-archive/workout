56a9295bf02e9a93da4cf0b47a57a8af3cf39dc0
Gram <master_fess@mail.ru>
2019-01-30 10:14:25 +0300

    +local settings

---
+++ project/settings/__init__.py
@@ -0,0 +1,9 @@
+from .base import *  # noqa: F401, F403
+
+try:
+    from .local import *  # noqa: F401, F403
+except ImportError:
+    from logging import getLogger
+    getLogger(__name__).warning('cannot import local settings, use settings for docker')
+
+    from .local_docker import *  # noqa: F401, F403
